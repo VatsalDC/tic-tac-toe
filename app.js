0e462da7886cd5637b47ca404252a1efdf3f3161
VatsalDC <vatsal.chevli.dv@gmail.com>
2024-08-09 17:18:35 +0530

    id's and classes changed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset-btn");
-let newGameBtn = document.querySelector("#new-btn");
+let resetBtn = document.querySelector(".reset-btn");
+let newGameBtn = document.querySelector(".new-btn");
 let msgContainer = document.querySelector(".msg-container");
-let msg = document.querySelector("#msg");
+let msg = document.querySelector(".msg");
 
 let turnO = true; //playerX, playerO
 let count = 0; //To Track Draw
